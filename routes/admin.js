a3d433a7d6fca653d79d6cb1b04062e28172c60a
BM Ferb <ferb@brickmate.vn>
2022-12-05 14:28:12 +0700

    - using es6+ syntax

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (_req, res) => {
-  res.render('index', { title: 'Express' });
+/* GET users listing. */
+router.get('/', (req, res) => {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.send('respond with a resource');
+});
+
+router.post('/signup', (req, res) => {
+  const data = req.body;
+  res.json(data);
 });
 
 module.exports = router;
