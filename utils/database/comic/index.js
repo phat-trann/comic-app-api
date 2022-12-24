db14efa27b854ba2923d0e2b600215028c4a5148
phat.tran <phat.tran.fit@gmail.com>
2022-12-24 16:28:28 +0700

    - handle search

--- utils/database/comic/index.js
+++ utils/database/comic/index.js
@@ -12,22 +12,30 @@ const getComicsCount = async () => {
 };
 
 const getComics = async (data) => {
-  const { skip, limit, sort, sortType, categoryIn, categoryEx, ...searchData } =
-    data;
+  const {
+    skip,
+    limit,
+    sort,
+    sortType,
+    categoryIn,
+    categoryEx,
+    chaptersLength,
+    ...searchData
+  } = data;
 
-  if (searchData?.chaptersLength) {
-    searchData.chaptersLength = { $gte: searchData.chaptersLength };
+  if (chaptersLength) {
+    searchData.chaptersLength = { $gte: chaptersLength };
   }
 
   if (categoryIn) {
-    searchData.category = {
+    searchData['category.key'] = {
       $all: categoryIn.split(','),
     };
   }
 
   if (categoryEx) {
-    searchData.category = {
-      ...searchData.category,
+    searchData['category.key'] = {
+      ...searchData['category.key'],
       $nin: categoryEx.split(','),
     };
   }
