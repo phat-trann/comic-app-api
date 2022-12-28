48d7f19795a40ca7d589266cec3d561b3cf7ccfc
phat.tran <phat.tran.fit@gmail.com>
2022-12-28 11:29:57 +0700

    - removed warning

--- utils/database/connect.js
+++ utils/database/connect.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false);
+
 const connectDB = () => {
   try {
     mongoose.connect(process.env.DB_URL, () => console.log('Database was connected'));
